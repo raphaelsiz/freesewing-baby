44ead5366fe95958e4d957bd94a054af5a98a615
Raphael <rmorgansizemore@gmail.com>
2023-04-01 12:47:54 -0700

    removed commented out code that doesn't work

--- testing.js
+++ testing.js
@@ -7,50 +7,7 @@ import {pluginTheme as theme} from '@freesewing/plugin-theme'
 import fs from 'fs'
 
 dotenv.config()
-/*async function logIn() {
-    let login = await axios.post('https://backend.freesewing.org/login',{username: process.env.USER,password: process.env.PASS})
-    console.log(login.data.token)
-    return login.data.token;
-}
-async function createPerson() {
-    let token = await logIn();
-    console.log(token)
-    const person = await axios.post('https://backend.freesewing.org/people/jwt',
-        {
-          name: "Someone",
-          notes: "These are some notes",
-          measies: {
-            "chest": 930,
-            "neck": 360
-          },
-          public: true,
-          imperial: false,
-          img: 'https://static.freesewing.org/users/w/wuuxp/wuuxp.jpeg'
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }
-      )
-    //console.log(person.data)
-}
-async function createApiKey() {
-    let token = await logIn();
-    //console.log(token)
-    const key = await axios.post('https://backend.freesewing.org/apikeys/jwt', {
-        name: "indiMeasurements",
-        level: 3,
-        expiresIn: 86400
-    },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }
-      )
-    //console.log(key)
-}*/
+
 function createBrian(measurements,url) {
     const svg = new Brian({
         sa: 10,
